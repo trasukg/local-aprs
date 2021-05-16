de0e2e603cf2780176673ce724d3cd9d2596a2dc
Greg Trasuk <trasukg@trasuk.com>
2021-05-16 11:54:16 -0400

    Updated karma config and tests so that 'ng test' runs cleanly.

--- src/app/aprs-situation/aprs-situation.selectors.spec.ts
+++ src/app/aprs-situation/aprs-situation.selectors.spec.ts
@@ -4,9 +4,9 @@ import { selectAprsSituationState } from './aprs-situation.selectors';
 describe('AprsSituation Selectors', () => {
   it('should select the feature state', () => {
     const result = selectAprsSituationState({
-      [fromAprsSituation.aprsSituationFeatureKey]: {}
+      [fromAprsSituation.aprsSituationFeatureKey]: fromAprsSituation.initialState
     });
 
-    expect(result).toEqual({});
+    expect(result).toEqual(fromAprsSituation.initialState);
   });
 });
